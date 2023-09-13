d53070cf78176ab86ac1d3dee72e5fba67e7b1b9
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2023-09-13 15:42:05 +0800

    More changes to dimensions

--- src/components/EduNeuralNet.tsx
+++ src/components/EduNeuralNet.tsx
@@ -1,6 +1,7 @@
 import ReactFlow, { Background, Handle, Position, ReactFlowProvider, useReactFlow } from 'reactflow';
 import { Study } from '../types';
 import { Card } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
 import 'reactflow/dist/style.css';
 import '../assets/css/neuralnet.sass';
 
@@ -43,62 +44,73 @@ const nodeTypes = { nnNode: NeuralNetNode, infinityNode: InfinityNode };
 const Flow = (props: { education: Study[] }) => {
     const reactFlowInstance = useReactFlow();
 
-    console.log(reactFlowInstance);
+    const [nodes, setNodes] = useState([]);
+    const [edges, setEdges] = useState([]);
 
-    const nodes = props.education.filter((edu) => edu.node > 0).map((edu) => ({
-        id: edu.node.toString(),
-        position: {
-            x: 100 * edu.node,
-            y: Number.isInteger(edu.node) ? 100 : 100 + 100 * (edu.node - Math.floor(edu.node))
-        },
-        type: 'nnNode',
-        data: {
-            label: edu.node, image: edu.image, alt: edu.institution, country: edu.country,
-            type: 'default'
-        },
-    }));
+    const [visibleNodes, setVisibleNodes] = useState([]);
 
-    nodes.push({
-        id: 'start',
-        position: { x: 0, y: 100 },
-        type: 'infinityNode',
-        data: { label: 0, alt: 'Early Life', type: 'source', country: '', image: '' },
-    });
-
-    nodes.push({
-        id: 'end',
-        position: { x: 100 * (props.education.length + 1), y: 100 },
-        type: 'infinityNode',
-        data: { label: 0, alt: 'Present', type: 'target', country: '', image: '' },
-    });
-
-    const edges = props.education.filter((edu) =>
-        Number.isInteger(edu.node) && edu.node > 0).map((edu) => ({
-            id: `${edu.node - 1}to${edu.node}`,
-            source: (edu.node - 1).toString(),
-            target: edu.node.toString(),
+    useEffect(() => {
+        const innerNodes = props.education.filter((edu) => edu.node > 0).map((edu) => ({
+            id: edu.node.toString(),
+            position: {
+                x: 100 * edu.node,
+                y: Number.isInteger(edu.node) ? 100 : 100 + 100 * (edu.node - Math.floor(edu.node))
+            },
+            type: 'nnNode',
+            data: {
+                label: edu.node, image: edu.image, alt: edu.institution, country: edu.country,
+                type: 'default'
+            },
         }));
 
-    edges.push({
-        id: `startto1`,
-        source: 'start',
-        target: '1',
-    });
+        const outerNodes = [{
+            id: 'start',
+            position: { x: 0, y: 100 },
+            type: 'infinityNode',
+            data: { label: 0, alt: 'Early Life', type: 'source', country: '', image: '' },
+        },
+        {
+            id: 'end',
+            position: { x: 100 * (props.education.length + 1), y: 100 },
+            type: 'infinityNode',
+            data: { label: 0, alt: 'Present', type: 'target', country: '', image: '' },
+        }];
 
-    edges.push({
-        id: `${props.education.length}toend`,
-        source: '3',
-        target: 'end',
-    });
+        setNodes(outerNodes.concat(innerNodes));
 
-    const visibleNodes = nodes.filter((node) => !isNaN(parseFloat((node.id))))
-    console.log(visibleNodes)
+        const innerEdges = props.education.filter((edu) =>
+            Number.isInteger(edu.node) && edu.node > 0).map((edu) => ({
+                id: `${edu.node - 1}to${edu.node}`,
+                source: (edu.node - 1).toString(),
+                target: edu.node.toString(),
+            }));
+
+        const outerEdges = [{
+            id: `startto1`,
+            source: 'start',
+            target: '1',
+        },
+        {
+            id: `${props.education.length}toend`,
+            source: '3',
+            target: 'end',
+        }];
+
+        setEdges(outerEdges.concat(innerEdges));
+
+        setVisibleNodes(innerNodes);
+        console.log(outerNodes.concat(innerNodes));
+
+        reactFlowInstance.fitBounds({ x: 150, y: 0, width: 200, height: 200 });
+
+    }, [props.education]);
+
+    console.log(reactFlowInstance);
 
     return (
         <Card variant='elevated' style={{ width: '100vw', height: '100vh' }}>
             <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes}
-                fitView={true} snapToGrid={true}
-                fitViewOptions={{ nodes: visibleNodes }}
+                snapToGrid={true}
                 panOnDrag={false} panOnScroll={false}
                 zoomOnScroll={false} zoomOnPinch={false} zoomOnDoubleClick={false}>
                 <Background />
